84f6568a0c3876d12f1b1ce4823de21d2d571aaa
immortalillusions <84871548+immortalillusions@users.noreply.github.com>
2025-04-19 18:15:09 -0400

    updated git user

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,7 @@
 "use client"; // This component is a client component
 export default function Profile() {
     return (
-      <div className="flex flex-col gap-16 justify-center items-center h-screen">
+      <div className="flex flex-col gap-16 justify-center items-center w-full h-screen bg-[url('/background_surf.png')] bg-cover bg-center">
         <h1 className="text-3xl font-bold font-sans">Profile Page</h1>
         <p className="text-lg">This is the pppp page</p>
         <img
